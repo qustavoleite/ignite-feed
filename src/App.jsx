424642859865b4012d7c0bc352b6b25221d8d4b1
qustavoleite <qustavoleite@gmail.com>
2024-12-24 14:35:56 -0300

    structure to separate feed and sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,27 @@
 import { Header } from './components/Header'
 import { Post } from './Post'
 import './global.css'
+import styles from './App.module.css'
 
 export function App() {
   return (
     <div>
-      <Header/>
+      <Header />
 
-      <Post
-        author='Gustavo Leite'
-        content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam sequi, deleniti rerum voluptate eligendi pariatur? Molestiae'
-      />
+      <div className={styles.wrapper}>
+        <aside>sidebar</aside>
+        <main>
+          <Post
+            author='Gustavo Leite'
+            content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam sequi, deleniti rerum voluptate eligendi pariatur? Molestiae'
+          />
 
-      <Post
-        author='Luis Henrique'
-        content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam sequi, deleniti rerum voluptate eligendi pariatur? Molestiae'
-      />
+          <Post
+            author='Gustavo Leite'
+            content='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Laboriosam sequi, deleniti rerum voluptate eligendi pariatur? Molestiae'
+          />
+        </main>
+      </div>
     </div>
-  )}
+  )
+}
