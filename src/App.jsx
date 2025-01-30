0a189a0967f7e0805c6ce0eb87c9c5b366fad150
qustavoleite <qustavoleite@gmail.com>
2025-01-30 16:22:27 -0300

    post component structure and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
-import { Post } from './Post'
+import { Post } from './components/Post'
 
 import './global.css'
 import styles from './App.module.css'
@@ -9,9 +9,7 @@ export function App() {
   return (
     <div>
       <Header />
-
       <div className={styles.wrapper}>
-
         <Sidebar/>  
 
         <main>
---
+++ src/components/Post.jsx
@@ -0,0 +1,46 @@
+import styles from './Post.module.css'
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src='https://github.com/qustavoleite.png'
+          />
+          <div className={styles.authorInfo}>
+            <strong>Gustavo Leite</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title='11 de Maio às 8:21' dateTime='2025-01-30 08:21:54'>
+          Publicado à 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>
+          <p>Fala galeraa 👋</p>
+
+          <p>
+            Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+            no NLW Return evento da Rocketseat. O nome do projeto é DoctorCare
+            🚀
+          </p>
+
+          <p>
+            <a href=''> jane.design/doctorcare</a>
+          </p>
+
+          <p>
+            <a href=''>#novoprojeto</a>{' '}
+            <a href=''>#nlw</a>{' '}
+            <a href=''>#rocketseat</a>
+          </p>
+        </p>
+      </div>
+    </article>
+  )
+}
